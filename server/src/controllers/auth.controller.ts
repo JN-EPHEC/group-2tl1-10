95b3e72f5fd3231a19c90307e21bbfb7c988da24
Gloutamax <justin.daub.fr@gmail.com>
2026-04-07 15:07:11 +0200

    #8 Auth : Méthode register

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,9 +1,50 @@
-import type { Request, Response} from 'express';
+import type { NextFunction, Request, Response} from 'express';
+import bcrypt from 'bcrypt';
+import { error } from 'node:console';
+import { hash } from 'node:crypto';
+// TODO : Importation du modèle sequelize des utilisateurs
+// import User from '../models/user.model';
 
 // Inscription 
-export const register = async (req: Request, res: Response) => {
-    // TODO: Récupérer l'email/password, hasher le MDP, sauver en DB
-    res.status(200).json({message: "Bouchon: Route Register OK"});
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, pseudo, password } = req.body;
+
+        if (!email || !pseudo || !password) {
+            return res.status(400).json({ error: "Tous les champs sont requis. "});
+        }
+
+        // Syntaxe Sequelize pour chercher un utilisateur
+        const existingUser = await User.findOne({
+            where: {email: email }
+        });
+
+        if (existingUser) {
+            return res.status(409).json({ error: "Cet email est déjà utilisé."});
+        }
+
+        const saltRounds = 10; 
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Syntaxe Sequelize pour créer un utilisateur 
+        const newUser = await User.create({
+            email: email,
+            pseudo: pseudo,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({
+            message: "Joueur créé avec succès, prêt pour le quiz !",
+            user: {
+                id: newUser.id, // Avec Sequelize, id est généralement auto-généré
+                email: newUser.email,
+                pseudo: newUser.pseudo
+            }
+        });
+        
+    } catch(error) {
+        next(error);
+    }
 } ;
 
 // Connexion 
