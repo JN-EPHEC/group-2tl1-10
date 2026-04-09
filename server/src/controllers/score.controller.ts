413b8a25fa908a839921d68de84bca4ee20fc826
Lucario <yonas.eberhart@gmail.com>
2026-04-09 14:56:21 +0200

    added types because ts complains a lot

--- server/src/controllers/score.controller.ts
+++ server/src/controllers/score.controller.ts
@@ -4,20 +4,20 @@ export const getTopScores = async (req: Request, res: Response, next: NextFuncti
     try {
         //code some shit in hereeeee
         const {scores} = req.body;
-        let output=[];
+        let output:number[] = [];
         for(let score of scores){ //look through all scores
             if(output.length<10){
                 output.push(score); //add any score if output not yet full
             }else{ //check if score replaces existing score v
-                let min=Infinity;
-                let index=0;
+                let min:number = Infinity;
+                let index:number = 0;
                 for(let out in output){ //look through current top scores
-                    if(output[out]<min){ //remember lowest score in output
+                    if(typeof(output[out])=="number" && output[out]<min){ //remember lowest score in output
                         min=output[out];
                         index=out;
                     }
                 }
-                if(min>score){ //replace lowest output with new score
+                if(min>score){ //replace lowest output score with new score
                     output.splice(index,1);
                     output.push(score);
                 }
