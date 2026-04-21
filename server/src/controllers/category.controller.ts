d2a308f54a2416d671084b2827e05702e1eec28e
Justin Daub <justin.daub.fr@gmail.com>
2026-04-21 10:46:26 +0200

    #8 Category : Méthode createCategory

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -1,8 +1,28 @@
 import type { Request, Response, NextFunction } from 'express';
 import { Category } from '../models';
 
-export const createCategory = (req: Request, res: Response) => {
-    res.status(200).json({ message: "Méthode createCategory"});
+// Créer une catégorie
+export const createCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, description } = req.body;
+
+        if (!name) {
+            return res.status(400).json({ error: "Le nom de la catégorie est obligatoire." });
+        }
+
+        const newCategory = await Category.create({ name, description });
+
+        return res.status(201).json({
+            message: "Catégorie créée avec succès !",
+            category: newCategory
+        });
+    } catch (error: any) {
+        // Gestion des doublons (si on essaie de créer "Histoire" deux fois)
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(409).json({ error: "Cette catégorie existe déjà." });
+        }
+        next(error);
+    }
 };
 
 export const getAllCategories = (req: Request, res: Response) => {
