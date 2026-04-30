7266fe0a1404c67b6e6e27f8af1483c5e8a07f12
Justin Daub <justin.daub.fr@gmail.com>
2026-04-30 12:46:50 +0200

    #6 Fichier de connexion des vues

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// TODO : Import des vues pricipal (Home, View, et le meilleur BRUH :) )
+// import HomeView from '../views/HomeView.vue'
+// import LoginView from '../views/LoginView.vue'
+// import BruhView from '../views/BruhView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/bruh',
+      name: 'bruh',
+      component: BruhView
+    }
+  ]
 })
 
 export default router
