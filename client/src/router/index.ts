142d253529bfcfc7ca97ecebe7abde65df949b90
Justin Daub <justin.daub.fr@gmail.com>
2026-04-30 14:02:21 +0200

    #6 Import des vues (Home,Login, Bruh

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// TODO : Import des vues pricipal (Home, View, et le meilleur BRUH :) )
-// import HomeView from '../views/HomeView.vue'
-// import LoginView from '../views/LoginView.vue'
-// import BruhView from '../views/BruhView.vue'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import BruhView from '../views/BruhView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
