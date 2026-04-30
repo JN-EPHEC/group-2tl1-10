276c240e13722368f11224fcf26729a40e258920
Justin Daub <justin.daub.fr@gmail.com>
2026-04-30 13:30:25 +0200

    #6 App.vue avec Login et Bruh

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <router-view /> </template>
 
-<style scoped></style>
+<style>
+/* Reset de base pour enlever les marges par défaut du navigateur */
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
