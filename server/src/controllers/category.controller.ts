b57cda73d371fbf38e0acda8e3831d4074c19a8f
Justin Daub <justin.daub.fr@gmail.com>
2026-05-06 18:10:28 +0200

    #5 Ajustement méthode createCategory pour les nouvelles options de création de quiz

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -1,25 +1,68 @@
 import type { Request, Response, NextFunction } from 'express';
-import { Category } from '../models';
+import { Category } from '../models/category.model';
+import { Question } from '../models/question.model';
+import { Answer } from '../models/playerAnswer.model'
+// TODO : Nouvelle table pour les paramètres a créer et à importer
+// import { Setting } from '../models/setting.model'
 
 // Créer une catégorie
 export const createCategory = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { name, description } = req.body;
+        const { name, description, questions } = req.body;
 
         if (!name) {
             return res.status(400).json({ error: "Le nom de la catégorie est obligatoire." });
         }
 
+        // Sauvegarde du Quiz (Category/Quiz)
         const newCategory = await Category.create({ name, description });
 
+        // On vérifie si le frontend a bien envoyé des questions
+        if (questions && Array.isArray(questions) && questions.length > 0) {
+
+            for (const q of questions) {
+                // Sauvegarde des questions 
+                const newQuestion = await Question.create({
+                    text: q.text,
+                    timeLimit: q.timeLimit,
+                    categoryId: newCategory.id // Clé étrangère permettant la relation
+                });
+
+                // Sauvegarde des réponses
+                if (q.answers && Array.isArray(q.answers)) {
+                    for (const ans of q.answers) {
+                        await Answer.create({
+                            text: ans.text,
+                            isCorrect: ans.isCorrect,
+                            questionId : newQuestion.id // Clé étrangère permettant de relier à la question 
+                        });
+                    }
+                }
+
+                // Sauvegarde des paramètres (sons, rage quit...)
+                if (q.settings) {
+                    await Setting.create({
+                        rageQuit: q.settings.rageQuit,
+                        secretButton: q.settings.secretButton,
+                        scoreMultiplier: q.settings.scoreMultiplier,
+                        winSound: q.settings.winSound, 
+                        firstWinSound: q.settings.firstWinSound,
+                        loseSound: q.settings.loseSound,
+                        firstLoseSound: q.settings.firstLoseSound,
+                        questionId: newQuestion.id // Clé étrangère avec la question
+                    });
+                }
+            }
+        }
+
         return res.status(201).json({
-            message: "Catégorie créée avec succès !",
+            message: "Quiz, crée avec succès !",
             category: newCategory
         });
     } catch (error: any) {
-        // Gestion des doublons (si on essaie de créer "Histoire" deux fois)
+        // Gestion des doublons
         if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(409).json({ error: "Cette catégorie existe déjà." });
+            return res.status(409).json({ error: "Ce quiz existe déjà." });
         }
         next(error);
     }
