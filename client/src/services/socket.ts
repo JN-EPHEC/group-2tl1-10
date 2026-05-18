9531d99d592ea351bdc7e4c82fb644d5a023a616
Justin Daub <justin.daub.fr@gmail.com>
2026-05-18 16:12:56 +0200

    #4 Correction ports pour les sockets

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 // Vite détecte automatiquement si on est en "Production" (sur le VPS) ou en local
 const URL = import.meta.env.PROD 
-  ? "http://www.l1-10.ephec-ti.be:3000" 
-  : "http://localhost:3000";
+  ? "http://www.l1-10.ephec-ti.be:5173" 
+  : "http://localhost:5173";
 
 export const socket = io(URL, {
     autoConnect: false // On ne se connecte pas automatiquement au chargement du site, cela sera fait manuellement lorsque quand on rentre dans le jeu
