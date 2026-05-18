12d4682f9a6ddd15ca9273e2c5d0c50cf8178739
Justin Daub <justin.daub.fr@gmail.com>
2026-05-18 16:11:15 +0200

    #4 Autorisation des sockets sur le site web

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-// URL et port du backend Node.js
-const URL = "http://localhost:3000";
+// Vite détecte automatiquement si on est en "Production" (sur le VPS) ou en local
+const URL = import.meta.env.PROD 
+  ? "http://www.l1-10.ephec-ti.be:3000" 
+  : "http://localhost:3000";
 
 export const socket = io(URL, {
     autoConnect: false // On ne se connecte pas automatiquement au chargement du site, cela sera fait manuellement lorsque quand on rentre dans le jeu
